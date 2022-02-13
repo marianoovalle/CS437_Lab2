a9e9b863090172168dd1f99608ff6e945b3651d5
Mariano-Macbook Pro <lo22@illinois.edu>
2022-02-13 11:16:33 -0800

    wifi ip updated

--- iot-lab-2/electron/index.js
+++ iot-lab-2/electron/index.js
@@ -2,7 +2,7 @@ document.onkeydown = updateKey;
 document.onkeyup = resetKey;
 
 var server_port = 65432;
-var server_addr = "192.168.3.49";   // the IP address of your Raspberry PI
+var server_addr = "192.168.1.35";   // the IP address of your Raspberry PI
 
 function client(){
     
